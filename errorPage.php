151eea2b718f0c0a22bead38df53f8e8a3d959e3
alencvitkovic <alencvitkovic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-27 20:32:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/stop-ie6/tags/1.03@268231 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ errorPage.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title><?php bloginfo('name'); ?></title>
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<meta name="Author" content="Alen Cvitkovic" />
+<meta name="generator" content="WordPress" />
+<link href="<?php echo get_option('home');?>/wp-content/plugins/stop-ie6/style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+	<div id="wrap" class="clear">
+		<div id="container" class="clear">
+			<div id="holder">
+				<div id="entry" class="clear">
+					<p>Your Internet browser is Internet Explorer 6.</p>
+					<p>You need a modern internet browser for viewing these pages.</p>
+					<p>Please update your present browser using <a href="http://update.microsoft.com/">Windows Update</a> or choose another one:</p>
+				</div>
+			</div>
+			
+			<div id="icons" class="clear">
+				<div class="logo">
+					<a href="http://www.mozilla.com/firefox/"><img src="<?php echo get_option('home');?>/wp-content/plugins/stop-ie6/images/firefox.png" alt="Firefox" /></a>
+					<p><a href="http://www.mozilla.com/firefox/">Download</a></p>
+				</div>
+				
+				<div class="logo">
+					<a href="http://www.apple.com/safari/download/"><img src="<?php echo get_option('home');?>/wp-content/plugins/stop-ie6/images/safari.png" alt="Safari" /></a>
+					<p><a href="http://www.apple.com/safari/download/">Download</a></p>
+				</div>
+				
+				<div class="logo">
+					<a href="http://www.google.com/chrome/"><img src="<?php echo get_option('home');?>/wp-content/plugins/stop-ie6/images/chrome.png" alt="Chrome" /></a>
+					<p><a href="http://www.google.com/chrome/">Download</a></p>
+				</div>
+				
+				<div class="logo">
+					<a href="http://www.opera.com/download/"><img src="<?php echo get_option('home');?>/wp-content/plugins/stop-ie6/images/opera.png" alt="Opera" /></a>
+					<p><a href="http://www.opera.com/download/">Download</a></p>
+				</div>
+				
+				<div class="logo">
+					<a href="http://www.microsoft.com/windows/internet-explorer/default.aspx"><img src="<?php echo get_option('home');?>/wp-content/plugins/stop-ie6/images/ie.png" alt="Internet Explorer 8" /></a>
+					<p><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx">Download</a></p>
+				</div>
+			</div><!-- Icons close -->
+		</div><!-- Container close -->
+	</div><!-- Wraper close -->
+</body>
+</html>
